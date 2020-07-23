2fb97b864df6d3c2e190b5a77934320d088253f8
vakyla98 <vakyla98@ukr.net>
2020-07-23 17:19:15 +0300

    Delete login banner at the bottom

--- injection.js
+++ injection.js
@@ -3,22 +3,28 @@ const config = { attributes: true };
 
 console.log("Injected");
 
+//Deleting starter login banner at the bottom
+let loginBanner = document.querySelector(".glyphsSpriteGrey_Close.u-__7")
+if(loginBanner) loginBanner.click();
+
+
 let checker = function () {
   target.style.overflow = "scroll";
   let presentations = document.querySelectorAll("[role=presentation]");
   presentations.forEach((elem) => {
-    console.log("x", elem.children);
     if (elem.children.length === 2) {
       elem.remove();
     }
   });
 };
 
-document.addEventListener("click", (e) => {
-  console.log(
-    document.querySelectorAll("[role=menuitem]")[0].childNodes[0].innerHTML
-  );
-});
+// document.addEventListener("click", (e) => {
+//   console.log(
+//     document.querySelectorAll("[role=menuitem]")[0].childNodes[0].innerHTML
+//   );
+// });
+
+//Закрыть/Закрити/Close
 
 const observer = new MutationObserver(checker);
 

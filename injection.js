85c69e6a9452069ddcaaf6968a757fbfc8e38cca
vakyla98 <vakyla98@ukr.net>
2020-07-24 16:01:38 +0300

    Creating new iframe when user click at post

--- injection.js
+++ injection.js
@@ -1,11 +1,10 @@
 console.log('Injected')
 
 const config = { attributes: true }
-
+//deleting blocking overlay
 const observerHandler = function () {
     document.body.style.overflow = 'scroll'
     let presentations = document.querySelectorAll('[role=presentation]')
-    //deleting blocking overlay
     presentations.forEach((elem) => {
         if (elem.children.length === 2) {
             elem.remove()
@@ -17,8 +16,17 @@ observer.observe(document.body, config)
 
 let posts = document.querySelector('article')
 function clickOnPostHandler(e) {
-    let link = e.target.closest('a')
-    console.log('From handler:', link.href)
+    let link = e.target.closest('a').href
+    console.log('From handler:', link)
+
+    let postNode = document.createElement('iframe')
+    postNode.src = link
+    postNode.style.cssText = `
+        position:absolute;
+        z-index:1;
+        width:800px;
+        height:400px;
+    `
+    document.body.prepend(postNode)
 }
 posts.addEventListener('click', clickOnPostHandler, false)
-
